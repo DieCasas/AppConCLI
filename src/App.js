9ff0111edca00cde2e9de5c90eb9fb62bb54ed59
Diego Casas <casas.adiego@gmail.com>
2022-08-23 22:30:01 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import hambur from './hambur.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={hambur} className="App-logo" alt="logo" />
-        <p>
-          Futuro E-Commerce sobre comida
-        </p>
-        <p>
-          Diego Casas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Don´t Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Componentes
+import NavBar from './Componentes/NavBar';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <NavBar />
+
+      </div>
+
+
+    )
+  }
 }
 
 export default App;
---
+++ src/Componentes/NavBar.js
@@ -0,0 +1,29 @@
+import '../App.css'
+const navBar = () => {
+    return (
+        <nav>
+            <div class="containerLista">  
+                <ul class="lista">
+                    <div>
+                        <li>Comidas</li>
+                        <i class="fa-solid fa-angle-down futuroAbrirModal"></i>
+                    </div>    
+                    <li>Ofertas</li>
+                    <li>Nosotros</li>
+                </ul>
+            </div> 
+            
+        </nav>
+    )
+}
+
+export default navBar;
+
+
+
+
+
+
+
+
+
